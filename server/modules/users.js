601e097dcb8d8cd1d8e72a4ccb881f757a2d5861
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2017-10-05 13:37:04 -0400

    added a post api method

---
+++ server/modules/users.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User', {
+
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        trim: true
+    }
+
+
+});
+
+module.exports = { User };
---
+++ server/server.js
@@ -0,0 +1,34 @@
+var { Mongoose } = require('./db/mongoose.js');
+var { Todo } = require('./modules/todos');
+var { User } = require('./modules/users');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.post('/todos', (req, res) => {
+
+    var todo = new Todo({
+
+        text: req.body.text,
+        completed: req.body.completed
+    });
+    todo.save().then((doc) => {
+        res.status(200).send(doc);
+        console.log(JSON.stringify(doc));
+    }, (err) => {
+
+        res.status(400).send(doc);
+    });
+
+
+
+
+})
+
+
+
+app.listen(3000, () => {
+    console.log('started at 3000 .')
+});
