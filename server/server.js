f8fe5aa57cc60e5307659007506abd5399715414
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2017-10-07 12:51:38 -0400

    added get http request passing a prameter

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var { Todo } = require('./modules/todos');
 var { User } = require('./modules/users');
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const { ObjectID } = require('mongodb');
 var app = express();
 
 app.use(bodyParser.json());
@@ -30,6 +30,22 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find()
         .then((todosList) => { res.send(JSON.stringify({ todosList })) }, (err) => { res.status(400).send(JSON.stringify(err)) })
+});
+app.get('/todos/:id', (req, res) => {
+
+    var id = req.params.id
+    if (!ObjectID.isValid(id)) { return res.status(404).send(); }
+    Todo.findById(id)
+        .then((doc) => {
+            if (!doc) { res.status(404).send(); }
+            res.send({ doc })
+        })
+        .catch((e) => {
+            res.status(404).send();
+        });
+
+
+
 })
 
 
