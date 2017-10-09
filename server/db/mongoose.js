2c746df02e4380ed138cc1af961f9529a07e5f54
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2017-10-09 13:12:42 -0400

    heruko error fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 const config = require('./../config/config');
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-if (config.env == 'development') { mongoose.connect('mongodb://localhost:27017/Todos'); } else if (config.env == 'development') {
+if (config.env == 'development') {
+    mongoose.connect(process.env.MONGODB_URI) || mongoose.connect('mongodb://localhost:27017/Todos');
+} else if (config.env == 'development') {
     mongoose.connect('mongodb://localhost:27017/Todos');
-} else { mongoose.connect(process.env.MONGODB_URI) }
+}
 
 module.exports = { mongoose };
