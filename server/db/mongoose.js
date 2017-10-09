6b26166b713bba3bc72ebaee33676da87d4eb004
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2017-10-09 13:30:18 -0400

    nothing serious

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,7 @@
-const config = require('./../config/config');
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-if (config.env == 'development') {
-    mongoose.connect(process.env.MONGODB_URI) || mongoose.connect('mongodb://localhost:27017/Todos');
-} else if (config.env == 'test') {
-    mongoose.connect('mongodb://localhost:27017/Todos');
-}
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Todos');
+
 
 module.exports = { mongoose };
